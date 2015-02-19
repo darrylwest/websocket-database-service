0a414b29cb00a16e8d716c96d32896dcb8f31ffd
darryl west <darryl.west@RainCitySoftware.com>
2015-02-18 18:11:20 -0800

    refactored db-client to check for ack

--- examples/db-client.js
+++ examples/db-client.js
@@ -16,57 +16,60 @@ var dash = require('lodash'),
     MessageHub = require( 'node-messaging-commons' ),
     hub = MessageHub.createInstance( config ),
     consumer = hub.createConsumer( DatabaseAccessService.DEFAULT_CHANNEL ),
+    privateChannel = '/test-' + (dash.random( 1000000 ) + 1000000).toString( 19 ),
     producer;
 
-var openDatabaseChannel = function() {
+var sendCommands = function() {
+
     var ssid = uuid.v4(),
-        request = {},
+        commandList = createCommandList(),
+        thread,
         rid = 1;
 
+    console.log('create the producer: ', privateChannel, ', session: ', ssid);
+    producer = hub.createProducer( privateChannel, ssid );
+
+    producer.onMessage(function(msg) {
+        if (msg.ssid !== ssid) {
+            console.log('received: ', JSON.stringify( msg ));
+        }
+    });
+
+    thread = setInterval(function() {
+        var command = commandList.shift(),
+            msg = {
+                "rid":rid++,
+                "cmd":command
+            };
+
+        if (command) {
+            console.log('send message: ', JSON.stringify( msg ));
+
+            producer.publish( msg, ssid );
+        } else {
+            clearInterval( thread );
+            thread = null;
+
+            producer.close();
+            consumer.close();
+
+            process.nextTick(function() {
+                process.kill( process.pid );
+            });
+        }
+    }, 1000);
+
+};
+
+var openDatabaseChannel = function() {
+    var request = {};
+
     request.action = 'openPrivateChannel';
-    request.privateChannel = '/test-' + (dash.random( 1000000 ) + 1000000).toString(19);
+    request.privateChannel = privateChannel;
 
     console.log('create the database channel: ', request);
 
     consumer.publish( request, config.appkey );
-
-    process.nextTick(function() {
-        var commandList = createCommandList(),
-            thread;
-
-        console.log('create the producer: ', request.privateChannel, ', session: ', ssid);
-        producer = hub.createProducer( request.privateChannel, ssid );
-
-        producer.onMessage(function(msg) {
-            if (msg.ssid !== ssid) {
-                console.log('received: ', JSON.stringify( msg ));
-            }
-        });
-
-        thread = setInterval(function() {
-            var command = commandList.shift(),
-                msg = {
-                    "rid":rid++,
-                    "cmd":command
-                };
-
-            if (command) {
-                console.log('send message: ', JSON.stringify( msg ));
-
-                producer.publish( msg, ssid );
-            } else {
-                clearInterval( thread );
-                thread = null;
-
-                producer.close();
-                consumer.close();
-
-                process.nextTick(function() {
-                    process.kill( process.pid );
-                });
-            }
-        }, 1000);
-    });
 };
 
 var createCommandList = function() {
@@ -93,11 +96,20 @@ var createCommandList = function() {
 };
 
 consumer.onConnect(function(chan) {
-    console.log('c>> connected to database service: ', chan);
+    console.log('consumer>> connected to database service: ', chan);
     openDatabaseChannel();
 });
 
 consumer.onMessage(function(msg) {
-    console.log( msg );
+    // only log messages from the primary hub
+    if (msg.ssid === config.appkey) {
+        console.log( 'consumer>> ', JSON.stringify( msg ));
+
+        // check message to verify that the private channel was accepted
+        if (true) {
+            sendCommands();
+        }
+    }
 });
 
+console.log('waiting for database consumer connection...');
